4c9bc1ef8628aa00880a54242ac2657aa0d17e94
hhmadan <hhmadan03@gmail.com>
2022-11-02 00:23:17 +0530

    UC2_CreateLinkedListByAddingNodes

--- src/linkedlistproblems/LinkedListDS.java
+++ src/linkedlistproblems/LinkedListDS.java
@@ -9,45 +9,37 @@ public class LinkedListDS {
         }
     }
     public Node head = null;
-    public void adding(int data) {
-            Node node = new Node(data);
-            if(head == null)
-            {
-                head = node;
-            }
-            else
-            {
-                Node temp = head;
-                while(temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            System.out.println(node.data+ " is Inserted In The Linked List");
-    }
-    public void display() {
-            Node temp = head;
-            if(temp == null)
-            {
-                System.out.println("LinkedList is Empty\n");
-                return;
-            }
-            while(temp != null)
-            {
-                System.out.print(temp.data + " ");
-                temp = temp.next;
-            }
-        }
-    public static void main(String[] args) {
-        System.out.println("WELCOME TO LINKED LIST PROGRAMS");
-        LinkedListDS list = new LinkedListDS();
-        list.display();
+    public Node tail = null;
 
-        list.adding(56);
-        list.adding(30);
-        list.adding(70);
-        System.out.println();
-        list.display();
+    public void addNode(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+        tail = newNode;
+    }
+    public void getDisplay(){
+        Node current = head;
+        if(head == null){
+            System.out.println("List is Empty");
+            return;
+        }
+        System.out.println("Nodes created in Linked List are: ");
+        while (current!= null){
+            System.out.print(current.data+" ");
+            current = current.next;
+        }
+    }
+    public static void main(String[] args) {
+        LinkedListDS list = new LinkedListDS();
+        list.getDisplay();
+        list.addNode(70);
+        list.addNode(30);
+        list.addNode(56);
+        list.getDisplay();
     }
 }
