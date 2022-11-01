30a26657277ae77135cb2ebfe935649017bbd420
hhmadan <hhmadan03@gmail.com>
2022-11-01 23:47:49 +0530

    Initial Commit

---
+++ src/linkedlistproblems/LinkedListDS.java
@@ -0,0 +1,7 @@
+package linkedlistproblems;
+
+public class LinkedListDS {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO LINKED LIST PROGRAMS");
+    }
+}
---
+++ src/linkedlistproblems/Node.java
@@ -0,0 +1,5 @@
+package linkedlistproblems;
+
+public class Node {
+
+}
